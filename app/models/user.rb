4e6b4ea3b0998c2b51fb51a20eb741bce1500b77
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2011-12-31 13:14:14 +0900

    Added admin flag to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login
 
   def self.find_for_database_authentication(warden_conditions)
-   conditions = warden_conditions.dup
-   login = conditions.delete(:login)
-   where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
- end
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111225083425) do
+ActiveRecord::Schema.define(:version => 20111231040313) do
 
   create_table "locations", :force => true do |t|
     t.string   "label"
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(:version => 20111225083425) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20111225083425) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.boolean  "admin",                                 :default => false, :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
