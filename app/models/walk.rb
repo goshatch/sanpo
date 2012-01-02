e3821424decc500b1fcc0af04b79a9fc193f4be9
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2012-01-03 01:50:16 +0900

    Experimental route display on map

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -2,4 +2,12 @@ class Walk < ActiveRecord::Base
   belongs_to :user
   has_many :locations
   has_many :photos
+
+  def center_coordinates
+    start = locations.first
+    goal = locations.last
+    target_latitude = (start.latitude + goal.latitude) / 2
+    target_longitude = (start.longitude + goal.longitude) / 2
+    {:latitude => target_latitude, :longitude => target_longitude}
+  end
 end
