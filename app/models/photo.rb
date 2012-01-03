24d8c6c543e70e46e270c050cdcae51bfffbefd0
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2012-01-04 00:16:48 +0900

    Making the walk page look better

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :walk
 
-  has_attached_file :image, :styles => { :large => "600x600", :medium => "x200", :thumb => "64x64#" }
+  has_attached_file :image, :styles => { :large => "620x620", :medium => "x200", :thumb => "64x64#" }
 end
