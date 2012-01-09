60281bc282feb10703ed68ec191c04eafbd249e4
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2012-01-09 15:46:42 +0900

    Messing around with footer and header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def usage_stats
+    users = User.count
+    walks = Walk.count
+    photos = Photo.count
+    t("general.stats", {:users => users, :walks => walks, :photos => photos})
+  end
 end
