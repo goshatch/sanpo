53c5295add091a344a2a66edaabeb3f7b9465ffa
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2012-01-12 00:03:39 +0900

    I am tired.

--- db/migrate/20120111145221_change_waypoints_float_precision.rb
+++ db/migrate/20120111145221_change_waypoints_float_precision.rb
@@ -1,10 +1,10 @@
 class ChangeWaypointsFloatPrecision < ActiveRecord::Migration
   def up
-    change_column :waypoints, :latitude, :float, :precision => 12, :scale => 16
-    change_column :waypoints, :longitude, :float, :precision => 12, :scale => 16
+    change_column :waypoints, :latitude, :float, :precision => 20, :scale => 16
+    change_column :waypoints, :longitude, :float, :precision => 20, :scale => 16
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    # raise ActiveRecord::IrreversibleMigration
   end
 end
