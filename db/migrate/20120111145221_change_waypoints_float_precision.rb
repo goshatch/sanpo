33f1ee478b5020666bccc852b2d20c4b530e8404
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2012-01-12 00:08:46 +0900

    Changed lat/long data types to decimal?

--- db/migrate/20120111145221_change_waypoints_float_precision.rb
+++ db/migrate/20120111145221_change_waypoints_float_precision.rb
@@ -1,7 +1,7 @@
 class ChangeWaypointsFloatPrecision < ActiveRecord::Migration
   def up
-    change_column :waypoints, :latitude, :float, :precision => 20, :scale => 16
-    change_column :waypoints, :longitude, :float, :precision => 20, :scale => 16
+    change_column :waypoints, :latitude, :decimal, :precision => 20, :scale => 16
+    change_column :waypoints, :longitude, :decimal, :precision => 20, :scale => 16
   end
 
   def down
