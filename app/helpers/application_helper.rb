652abb700debc2936260ddb2605d89f7a1707175
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2012-08-04 17:38:41 +0900

    Get user avatar from Twitter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,13 @@ module ApplicationHelper
       "#{number} comments"
     end
   end
+
+  def avatar_image_tag(user)
+    profile = user.profile
+    if profile.twitter
+      image_tag "https://api.twitter.com/1/users/profile_image?screen_name=#{profile.twitter}&size=bigger"
+    else
+      image_tag "default_avatar.png"
+    end
+  end
 end
