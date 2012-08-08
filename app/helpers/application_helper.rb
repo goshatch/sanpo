114d6b98337023a5d77d145aa5ac2e5a4bf95ac1
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2012-08-08 23:03:11 +0900

    Add link to profile in navigation bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,12 +17,12 @@ module ApplicationHelper
     end
   end
 
-  def avatar_image_tag(user)
+  def avatar_image_tag(user, size = "73x73")
     profile = user.profile
     if profile.twitter.present?
-      image_tag "https://api.twitter.com/1/users/profile_image?screen_name=#{profile.twitter}&size=bigger"
+      image_tag "https://api.twitter.com/1/users/profile_image?screen_name=#{profile.twitter}&size=bigger", :size => size
     else
-      image_tag "default_avatar.png"
+      image_tag "default_avatar.png", :size => size
     end
   end
 end
