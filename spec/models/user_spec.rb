1dfacca8dae9518fa9b8f9d0442233a3f4ea33e4
Charles Abbott <cwabbott@gmail.com>
2012-11-16 21:51:14 +0900

    a few specs, improved error handling, and explicity verify authentication with before filters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe ".save!" do
+    let(:valid_user) { FactoryGirl.build(:user) }
+    it "should save a valid user" do
+      valid_user.save!.should be_true
+    end
+    
+    it "should have an attached profile" do
+      valid_user.save!
+      valid_user.profile.should_not be_nil
+    end
+  end
 end
