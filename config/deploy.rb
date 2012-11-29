8050a650ec3fb4f1266813bf3d1234eb754c1d2d
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2012-11-29 23:26:13 +0900

    Remove precreate profiles rake task from deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,6 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-    run "rake precreate_profiles"
   end
 
   desc "Symlink shared config files and folders for each release"
