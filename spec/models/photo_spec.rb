2e94025e1bdbf2071eb1d24ef6e98cb71e1cb401
EmilRehnberg <emil.rehnberg@gmail.com>
2013-02-09 10:37:23 +0900

    Added some minimal model tests

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Photo do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_photo){ FactoryGirl.build(:photo) }
+  it "should save a valid photo" do
+    valid_photo.save!.should be_true
+  end
 end
