021b5de29dcb2f0b2091952b9c19a565732eb1ee
Gueorgui Tcherednitchenko <gueorgui@gmail.com>
2013-02-12 11:22:45 +0900

    Moved request specs to features directory
    This is caused by upgrading to Capybara 2.0
    https://github.com/rspec/rspec-rails/blob/master/Capybara.md

---
+++ spec/features/walks_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "WalkLists" do
+  describe "GET /walk_lists" do
+    it "works! (now write some real specs)" do
+      pending
+      # # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      # visit walks_path
+      # response.status.should be(200)
+    end
+  end
+end
